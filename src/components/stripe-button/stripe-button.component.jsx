8a595065c357deaa9a3d70bcee908647c10c9667
11kenken11 <kenji111.fujimoto@gmail.com>
2021-03-22 11:51:08 +0100

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import react from "react";
+import StripCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_MXHB0fZe8Ka9KP3WoDMHzY9f00Lrq0WDlb";
+
+  const onToken = (token) => {
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="http://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
