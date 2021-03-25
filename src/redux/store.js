7599100575117e01c1803ef1e7ed59d62bfb513b
11kenken11 <kenji111.fujimoto@gmail.com>
2021-03-25 12:56:09 +0100

    created user and cart sagas for sign in, sign up and sign in with google

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,26 @@
 import { createStore, applyMiddleware } from "redux";
 
 import logger from "redux-logger";
+//import thunk from "redux-thunk";
+import createSagaMiddleWare from "redux-saga";
+
+import rootSaga from "./root-saga";
 
 import rootReducer from "./root-reducer";
 
 import { persistStore } from "redux-persist";
 
-const middlewares = [];
+const sagaMiddleWare = createSagaMiddleWare();
+
+const middlewares = [sagaMiddleWare];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleWare.run(rootSaga);
 
 export const persistor = persistStore(store);
 
